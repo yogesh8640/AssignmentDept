01b3695311d7ca08c50f8ac6a4a3792a098ce3f2
Akanksha-git1 <1000073994@hexaware.com>
2022-09-01 10:44:10 +0530

    Login model is added

---
+++ AssignmentDept/Models/Login.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AssignmentDept.Models
+{
+    
+    public class Login
+    {
+        [Required]
+        public int username { get; set; }
+        [Required]
+        public string password { get; set; }
+    }
+}
